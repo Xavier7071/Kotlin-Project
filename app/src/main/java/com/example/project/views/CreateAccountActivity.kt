a416cb966837fae3f9536be73a5c1ac207d5ac60
Xavier Mathieu <xavier2003@hotmail.fr>
2021-11-25 17:36:17 -0500

    toute marche

--- app/src/main/java/com/example/project/views/CreateAccountActivity.kt
+++ app/src/main/java/com/example/project/views/CreateAccountActivity.kt
@@ -2,6 +2,7 @@ package com.example.project.Views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project.R
 
 class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
