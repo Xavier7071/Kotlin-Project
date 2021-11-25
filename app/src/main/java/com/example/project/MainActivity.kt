d0a7828324a2a1106e5daf9b65ae9c5bf369b1b4
Ludovick31 <83981459+Ludovick31@users.noreply.github.com>
2021-11-25 14:55:37 -0500

    Update MainActivity.kt
    asfgsagsgasgasgasgasg

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //TODO: Bonne chance xavier :)----
+        //TODO: Bonne chance xavier :)----xavier
     }
 }
