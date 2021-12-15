e1f086763b38825bd7dfb159665d6b45901374da
Xavier Mathieu <xavier2003@hotmail.fr>
2021-12-15 16:08:11 -0500

    test

--- app/src/main/java/com/example/project/adapter/TeamsListAdapter.kt
+++ app/src/main/java/com/example/project/adapter/TeamsListAdapter.kt
@@ -1,18 +1,23 @@
 package com.example.project.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.project.R
 import com.example.project.controllers.MainController
 import com.example.project.models.Teams
+import com.example.project.views.CreateGameActivity
+import com.example.project.views.TeamInfosActivity
 
 class TeamsListAdapter(private var list: ArrayList<Teams>) :
     RecyclerView.Adapter<TeamsListAdapter.ViewHolder>() {
-
+    private lateinit var context: Context
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val name: TextView = view.findViewById(R.id.teamName)
         val category: TextView = view.findViewById(R.id.teamLevel)
@@ -20,6 +25,7 @@ class TeamsListAdapter(private var list: ArrayList<Teams>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.team_info_layout, parent, false)
         )
@@ -40,6 +46,8 @@ class TeamsListAdapter(private var list: ArrayList<Teams>) :
 
     private fun launchTeamInfo(id: Int) {
         MainController.instance.setTeamId(id)
+        val intent = Intent(context, TeamInfosActivity::class.java)
+        startActivity(context, intent, null)
         //launch icitte l'activité de l'équipe avec l'id de l'équipe pour le query
     }
 }
