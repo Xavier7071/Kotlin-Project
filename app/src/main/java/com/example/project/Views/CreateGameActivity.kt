8e2259a5c324e9692dab639c5ba28076f9b1c3c6
Xavier Mathieu <xavier2003@hotmail.fr>
2021-12-15 16:38:18 -0500

    geryhe

--- app/src/main/java/com/example/project/Views/CreateGameActivity.kt
+++ app/src/main/java/com/example/project/Views/CreateGameActivity.kt
@@ -2,21 +2,29 @@ package com.example.project.views
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.*
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.project.R
 import com.example.project.controllers.MainController
 import com.example.project.models.Teams
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.log
 
 class CreateGameActivity : AppCompatActivity() {
+    private var savedYear: Int = 0
+    private var savedMonth: Int = 0
+    private var savedDay: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_game)
 
-        val teamsArray : ArrayList<Teams> = MainController.instance.getOtherTeams()
-        val teamNames : ArrayList<String> = ArrayList()
+        val teamsArray: ArrayList<Teams> = MainController.instance.getOtherTeams()
+        val teamNames: ArrayList<String> = ArrayList()
         teamsArray.forEach {
             teamNames.add(it.name)
         }
@@ -32,12 +40,16 @@ class CreateGameActivity : AppCompatActivity() {
         createGameBtn.setOnClickListener {
             validateGameInfo()
         }
-
-        // val datePickerBtn : TextView = findViewById(R.id.datePickerBtn)
-
-        //datePickerBtn.setOnClickListener {
-        //  TimePickerFragment().show(supportFragmentManager, "timePicker")
-        //}
+        val datePicker = findViewById<DatePicker>(R.id.datePicker)
+        val today = Calendar.getInstance()
+        datePicker.init(
+            today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { _, year, month, day ->
+            savedYear = year
+            savedMonth = month
+            savedDay = day
+        }
     }
 
     private fun validateGameInfo() {
@@ -45,15 +57,17 @@ class CreateGameActivity : AppCompatActivity() {
         val opponentSpinner: Spinner = findViewById(R.id.opponentSpinner)
         val location = locationSpinner.selectedItem.toString()
         val opponent = opponentSpinner.selectedItem.toString()
-
-        MainController.instance.insertGame(MainController.instance.getTeamByName(opponent), location, date = Date())
+        val date = Date()
+        date.year = savedYear - 1900
+        date.month = savedMonth
+        date.date = savedDay
+        MainController.instance.insertGame(
+            MainController.instance.getTeamByName(opponent),
+            location,
+            date
+        )
 
         val intent = Intent(this, AccountActivity::class.java)
         startActivity(intent)
     }
 }
-
-/*
-    date (date picker dialog)
-
- */
