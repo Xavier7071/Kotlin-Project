29e9f8ff45a7c81f777b5bdd772abecf6ad6321f
Xavier Mathieu <xavier2003@hotmail.fr>
2021-12-15 15:52:48 -0500

    Update CreateGameActivity.kt

--- app/src/main/java/com/example/project/Views/CreateGameActivity.kt
+++ app/src/main/java/com/example/project/Views/CreateGameActivity.kt
@@ -1,12 +1,12 @@
 package com.example.project.views
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.example.project.R
 import com.example.project.controllers.MainController
-import com.example.project.models.Teams
+import java.util.*
 
 class CreateGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,38 +14,34 @@ class CreateGameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_game)
 
         val opponentSpinner: Spinner = findViewById(R.id.opponentSpinner)
-        opponentSpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, MainController.instance.getOtherTeams())
-
+        opponentSpinner.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            MainController.instance.getOtherTeams()
+        )
 
         val createGameBtn: Button = findViewById(R.id.createGameBtn)
         createGameBtn.setOnClickListener {
             validateGameInfo()
         }
 
-       // val datePickerBtn : TextView = findViewById(R.id.datePickerBtn)
+        // val datePickerBtn : TextView = findViewById(R.id.datePickerBtn)
 
         //datePickerBtn.setOnClickListener {
-          //  TimePickerFragment().show(supportFragmentManager, "timePicker")
+        //  TimePickerFragment().show(supportFragmentManager, "timePicker")
         //}
-
     }
 
     private fun validateGameInfo() {
-        val hourInput: EditText = findViewById(R.id.hourInput)
         val locationSpinner: Spinner = findViewById(R.id.gameLocationSpinner)
         val opponentSpinner: Spinner = findViewById(R.id.opponentSpinner)
-        if (hourInput.text.toString().isEmpty()) {
-            Toast.makeText(this, "Veuillez entrer une heure pour la partie", Toast.LENGTH_SHORT).show()
-        } else {
-            val location = locationSpinner.selectedItem.toString()
-            val opponent = opponentSpinner.selectedItem.toString()
+        val location = locationSpinner.selectedItem.toString()
+        val opponent = opponentSpinner.selectedItem.toString()
 
-            MainController.instance.insertTeam(name.text.toString(), category, code)
-            MainController.instance.insertTeamUser(code)
+        MainController.instance.insertGame(MainController.instance.getTeamById(opponent), location, date = Date())
 
-            val intent = Intent(this, AccountActivity::class.java)
-            startActivity(intent)
-        }
+        val intent = Intent(this, AccountActivity::class.java)
+        startActivity(intent)
     }
 }
 
